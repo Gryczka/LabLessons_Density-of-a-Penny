8599e8f17ba210138f5ae13432de2fd380afdd1b
Gryczka <PatrickGryczka@gmail.com>
2016-06-03 13:00:51 -0400

    Further Basic Additions - Rotate Penny Button, Beaker w/ Water
    Further Basic Additions - Rotate Penny Button, Beaker w/ Water
    
    So far just beaker with water which penny can be placed in.  Still need
    to add script to raise water level as penny is submerged.
    
    Also need to add a ruler and have the measurements appear in the left
    panel.

---
+++ DensityOfAPenny/Assets/Scripts/Rotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    
+	public void RotatePenny () {
+        transform.Rotate(new Vector3(90f,0f,0f));
+	}
+	
+}
